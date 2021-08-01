0c85dbf2d53f0f2085bd2c171d94d1170876f7c4
bestcodderepos <bestcodder@outlook.com>
2021-08-01 14:47:37 +0300

    Blazor Traning Complete

---
+++ BestCodderCourse/Models/CourseTraning.cs
@@ -0,0 +1,10 @@
+﻿namespace BestCodderCourse.Models
+{
+    public class CourseTraning
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ BestCodderCourse/Models/DropdownProps.cs
@@ -0,0 +1,9 @@
+﻿namespace BestCodderCourse.Models
+{
+    public class DropdownProps
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }  
+    }
+}
